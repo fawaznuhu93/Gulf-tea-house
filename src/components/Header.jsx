687f3dc886e4e1f2406ad3fc23f6d31013c36da0
Fawaz Nuhu <fawaznuhu93@gmail.com>
2026-01-06 22:13:04 +0100

    update mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -112,7 +112,7 @@ const scrollToSection = (id) => {
                     initial={{ x: -20, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ delay: index * 0.1 }}
-                    onClick={() =>{scrollToSection(item.label.toLowerCase());
+                    onClick={() =>{scrollToSection(item.id);
                   
                     setIsOpen(false);}}
                     className="text-gray-700 dark:text-gray-300 hover:text-chai-600 dark:hover:text-chai-400 font-medium py-2 border-b border-gray-200 dark:border-gray-700"
